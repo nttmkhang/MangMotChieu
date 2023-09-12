9c0b198419272946ee7a0073f3af439a4c245a99
elodisme1 <22520254@gm.uit.edu.vn>
2023-09-12 15:17:32 +0700

    Do

--- Bai165/Bai165.cpp
+++ Bai165/Bai165.cpp
@@ -1,20 +1,64 @@
-// Bai165.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Nhap(float[], int&);
+void Xuat(float[], int);
+
+void XoaViTri(float[], int&, int);
+float LonNhat(float[], int);
+void XoaLonNhat(float[], int&);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float b[500];
+    int k;
+
+    cout << "Mang:\n";
+    Nhap(b, k);
+    cout << "Mang ban dau:";
+    Xuat(b, k);
+
+    XoaLonNhat(b, k);
+    cout << "\nMang sau khi xoa so lon nhat: ";
+    Xuat(b, k);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Nhap(float a[], int& n)
+{
+    cout << "Nhap n: ";
+    cin >> n;
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+        a[i] = -100.0 + (rand() / (RAND_MAX / (100.0 - (-100.0))));
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Xuat(float a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << setw(10) << setprecision(5) << a[i];
+}
+
+void XoaViTri(float a[], int& n, int k)
+{
+    for (int i = k; i <= n - 2; i++)
+        a[i] = a[i + 1];
+    n--;
+}
+
+float LonNhat(float a[], int n)
+{
+    float lc = a[0];
+    for (int i = 0; i < n; i++)
+        if (a[i] > lc)
+            lc = a[i];
+    return lc;
+}
+
+void XoaLonNhat(float a[], int& n)
+{
+    float ln = LonNhat(a, n);
+    for (int i = n - 1; i >= 0; i--)
+        if (a[i] == ln)
+            XoaViTri(a, n, i);
+}
