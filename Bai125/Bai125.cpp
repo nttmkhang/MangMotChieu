7840c1c7ea9b07f7c8965bf45c1fe2a1b08c99ff
chihuy124 <22520577@gm.uit.edu.vn>
2023-09-12 15:57:26 +0700

    huy

--- Bai125/Bai125.cpp
+++ Bai125/Bai125.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+int ktTang(float[], int);
+void Nhap(float[], int&);
+void Xuat(float[], int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	float a[1000];
+	Nhap(a, n);
+	Xuat(a, n);
+	int kq = ktTang(a, n);
+
+	if (kq == 0)
+		cout << "\nKhong tang";
+	else
+		cout << "\nTang";
 }
 
+int ktTang(float a[], int n)
+{
+    int flag = 1;
+    for (int i = 0; i <= n - 2; i++)
+        if (a[i] > a[i + 1])
+            flag = 0;
+    return flag;
+}
+
+void Nhap(float a[], int& n)
+{
+	cout << "Nhap n : ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i <= n - 1; i++)
+		a[i] = -100.0 + (rand() / (RAND_MAX / (100.0 - (-100.0))));
+}
+
+void Xuat(float a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
+}
--- Bai129/Bai129.cpp
+++ Bai129/Bai129.cpp
@@ -1,20 +1,53 @@
-// Bai129.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+int ktSong(int[], int);
+void Nhap(int[], int&);
+void Xuat(int[], int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	int a[1000];
+	Nhap(a, n);
+	Xuat(a, n);
+	int kq = ktSong(a, n);
+
+	if (kq == 0)
+		cout << "\nKhong dang song";
+	else
+		cout << "\La dang song";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int ktSong(int a[], int n)
+{
+	if (n <= 1)
+		return 0;
+	if (n == 2)
+		if (a[0] == a[1])
+			return 0;
+		else
+			return 1;
+	int flag = 1;
+	for (int i = 1; i <= n - 2; i++)
+		if ((a[i] - a[i - 1]) * (a[i] - a[i + 1]) <= 0)
+			flag = 0;
+	return flag;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n : ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i <= n - 1; i++)
+		a[i] = rand() % (200 + 1) - 100;
+}
+
+void Xuat(int a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
+}
