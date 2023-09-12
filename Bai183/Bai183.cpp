fa919590d1aa6e970d447d094ef191c5f158d14f
leetoneee <letoan7442@gmail.com>
2023-09-12 14:11:13 +0700

    Toan

--- Bai183/Bai183.cpp
+++ Bai183/Bai183.cpp
@@ -1,20 +1,29 @@
-// Bai183.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+void Nhap(int[], int&);
+void Xuat(int[], int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Nhap(int a[], int& n)
+{
+	cout << "Nhap n : ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i <= n - 1; i++)
+		a[i] = rand() % (200 + 1) - 100;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Xuat(int a[], int n)
+{
+	cout << n << endl;
+	for (int i = 0; i <= n - 1; i++)
+		cout << setw(10) << a[i];
+}
